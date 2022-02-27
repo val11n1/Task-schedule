79892327b14417e3cfa30cb8780816c58eda4ced
Valeriy Trusov <trusov11mail@mail.ru>
2022-02-27 14:43:31 +0300

    Initial Commit

---
+++ mySheduleDz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mySheduleDz
+//
+//  Created by Valeriy Trusov on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
